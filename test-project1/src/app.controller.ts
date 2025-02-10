2ba1db041ce1a424d9edcdd67d4a5f3483d92de8
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-09 23:52:12 -0500

    code

--- test-project1/src/app.controller.ts
+++ test-project1/src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+
+  @Get('about')
+  getAbout(): string {
+    return '<h1>About Page</h1>:';
+  }
 }
